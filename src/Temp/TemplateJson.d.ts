f2d40fd70dcd7ac85f88962a9ee962ba3662d584
DragonLords <70346417+DragonLords@users.noreply.github.com>
2022-09-03 09:09:53 -0400

    Delete src directory

--- src/Temp/TemplateJson.d.ts
+++
@@ -1,4 +0,0 @@
-export interface JsonTemplate{
-    Id:number;
-    Name:string
-}
--- src/global.d.ts
+++
@@ -1,7 +0,0 @@
-namespace NodeJS{
-    interface ProcessEnv {
-        PORT:number;
-        HOSTNAME:string;
-    }
-}
-global.curDir=process.cwd();
